f2b579e25fbe9bd1285749201f004ea091278191
Ivan Veselovsky <iveselovsky@griddynamics.com>
2013-04-08 21:32:02 +0400

    added TestBundleJobBean

--- core/src/test/java/org/apache/oozie/TestBundleJobBean.java
+++ core/src/test/java/org/apache/oozie/TestBundleJobBean.java
@@ -5,157 +5,111 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 
-import org.apache.oozie.client.CoordinatorJob.Execution;
-import org.apache.oozie.client.CoordinatorJob.Timeunit;
+import org.apache.oozie.client.BundleJob.Timeunit;
 import org.apache.oozie.client.Job.Status;
-import org.apache.oozie.client.rest.JsonCoordinatorAction;
 import org.apache.oozie.test.XTestCase;
 import org.apache.oozie.util.DateUtils;
 
-public class TestCoordinatorJobBean extends XTestCase {
+public class TestBundleJobBean extends XTestCase {
 
     private static final Date ct = new Date(1L);
     private static final Date et = new Date(2L);
     private static final Date st = new Date(3L);
-    private static final Date lat = new Date(4L);
+    private static final Date kt = new Date(4L);
     private static final Date lmt = new Date(5L);
-    private static final Date nmt = new Date(6L);
     private static final Date pt = new Date(7L);
     private static final Date spt = new Date(8L);
-    private static final List<? extends JsonCoordinatorAction> actionList = Collections
-            .singletonList(new CoordinatorActionBean());
-    private static final Execution execution = Execution.LIFO;
 
-    private void set(CoordinatorJobBean bean) {
-        bean.setActions(actionList);
+    private void set(BundleJobBean bean) {
         bean.setAppName("an");
-        bean.setAppNamespace("ans");
         bean.setAppPath("ap");
         bean.setAuthToken("at");
-        bean.setBundleId("bi");
-        bean.setConcurrency(2);
         bean.setConf("c");
         bean.setConsoleUrl("cu");
         bean.setCreatedTime(ct);
-        bean.setDoneMaterialization();
         bean.setEndTime(et);
         bean.setEndTimestamp(DateUtils.convertDateToTimestamp(et));
-        bean.setExecution(execution);
-        bean.setExecutionOrder(execution);
         bean.setExternalId("ei");
-        bean.setFrequency(3);
         bean.setGroup("group");
         bean.setId("id");
         bean.setJobXml("jx");
-        bean.setLastActionNumber(4);
-        bean.setLastActionTime(lat);
-        bean.setLastActionTimestamp(DateUtils.convertDateToTimestamp(lat));
+        bean.setKickoffTime(kt);
+        bean.setKickoffTimestamp(DateUtils.convertDateToTimestamp(kt));
         bean.setLastModifiedTime(lmt);
         bean.setLastModifiedTimestamp(DateUtils.convertDateToTimestamp(lmt));
-        bean.setMatThrottling(10);
-        bean.setNextMaterializedTime(nmt);
-        bean.setNextMaterializedTimestamp(DateUtils.convertDateToTimestamp(nmt));
         bean.setOrigJobXml("ojx");
         bean.setPauseTime(pt);
         bean.setPending();
-        bean.setSlaXml("sx");
         bean.setStartTime(st);
         bean.setStartTimestamp(DateUtils.convertDateToTimestamp(st));
         bean.setStatus(Status.KILLED);
         bean.setSuspendedTime(spt);
         bean.setSuspendedTimestamp(DateUtils.convertDateToTimestamp(spt));
-        bean.setTimeout(11);
+        bean.setTimeOut(11);
         bean.setTimeUnit(Timeunit.MINUTE);
-        bean.setTimeZone("GMT");
         bean.setUser("u");
     }
 
     /**
-     * Test {@link CoordinatorJobBean} get- and set- methods.
+     * Test {@link BundleJobBean} get- and set- methods.
      */
     public void testSetGet() {
-        final CoordinatorJobBean bean = new CoordinatorJobBean();
+        final BundleJobBean bean = new BundleJobBean();
         set(bean);
         testGet(bean, true);
     }
 
-    private void testGet(CoordinatorJobBean bean, boolean checkDeserialization) {
-        if (checkDeserialization) {
-            assertEquals(actionList, bean.getActions());
-        }
+    private void testGet(BundleJobBean bean, boolean checkDeserialization) {
         assertEquals("an", bean.getAppName());
-        assertEquals("ans", bean.getAppNamespace());
         assertEquals("ap", bean.getAppPath());
         if (checkDeserialization) {
             assertEquals("at", bean.getAuthToken());
-            assertEquals("bi", bean.getBundleId());
         }
-        assertEquals(2, bean.getConcurrency());
         assertEquals("c", bean.getConf());
         if (checkDeserialization) {
             assertEquals("cu", bean.getConsoleUrl());
             assertEquals(ct, bean.getCreatedTime());
         }
-        assertEquals(true, bean.isDoneMaterialization());
         assertEquals(et, bean.getEndTime());
         assertEquals(DateUtils.convertDateToTimestamp(et),
                 bean.getEndTimestamp());
-        if (checkDeserialization) {
-            assertEquals(execution.toString(), bean.getExecution());
-        }
-        assertEquals(execution, bean.getExecutionOrder());
         assertEquals("ei", bean.getExternalId());
-        assertEquals(3, bean.getFrequency());
         assertEquals("group", bean.getGroup());
         assertEquals("id", bean.getId());
         if (checkDeserialization) {
             assertEquals("jx", bean.getJobXml());
-            assertEquals(4, bean.getLastActionNumber());
         }
-        assertEquals(lat, bean.getLastActionTime());
-        assertEquals(DateUtils.convertDateToTimestamp(lat),
-                bean.getLastActionTimestamp());
+        assertEquals(kt, bean.getKickoffTime());
+        assertEquals(DateUtils.convertDateToTimestamp(kt),
+                bean.getKickoffTimestamp());
         if (checkDeserialization) {
             assertEquals(lmt, bean.getLastModifiedTime());
             assertEquals(DateUtils.convertDateToTimestamp(lmt),
                     bean.getLastModifiedTimestamp());
         }
-        assertEquals(10, bean.getMatThrottling());
-        assertEquals(nmt, bean.getNextMaterializedTime());
-        assertEquals(DateUtils.convertDateToTimestamp(nmt),
-                bean.getNextMaterializedTimestamp());
         if (checkDeserialization) {
             assertEquals("ojx", bean.getOrigJobXml());
             assertEquals(pt, bean.getPauseTime());
-        }
-        assertEquals(true, bean.isPending());
-        if (checkDeserialization) {
-            assertEquals("sx", bean.getSlaXml());
+            assertEquals(true, bean.isPending());
         }
         assertEquals(st, bean.getStartTime());
-        assertEquals(DateUtils.convertDateToTimestamp(st),
-                bean.getStartTimestamp());
         assertEquals(Status.KILLED, bean.getStatus());
         if (checkDeserialization) {
-            assertEquals(spt, bean.getSuspendedTime());
             assertEquals(DateUtils.convertDateToTimestamp(spt),
                     bean.getSuspendedTimestamp());
         }
         assertEquals(11, bean.getTimeout());
         assertEquals(Timeunit.MINUTE, bean.getTimeUnit());
-        assertEquals("GMT", bean.getTimeZone());
         assertEquals("u", bean.getUser());
     }
 
     /**
-     * Test {@link CoordinatorJobBean} serialization and deserialization.
+     * Test {@link BundleJobBean} serialization and deserialization.
      */
     public void testSerialization() throws IOException {
-        final CoordinatorJobBean bean = new CoordinatorJobBean();
+        final BundleJobBean bean = new BundleJobBean();
 
         set(bean);
 
@@ -164,7 +118,7 @@ public class TestCoordinatorJobBean extends XTestCase {
         bean.write(dos);
         dos.close();
 
-        final CoordinatorJobBean bean2 = new CoordinatorJobBean();
+        final BundleJobBean bean2 = new BundleJobBean();
         bean2.readFields(new DataInputStream(new ByteArrayInputStream(baos
                 .toByteArray())));
 
