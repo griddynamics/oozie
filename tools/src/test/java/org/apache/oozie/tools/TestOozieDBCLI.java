28841fb849809f8e5bb7a36d9073dc1757d1523d
aleksgor <agorshkov@griddynamics.com>
2013-04-15 11:56:17 +0400

    add test

--- tools/src/test/java/org/apache/oozie/tools/TestOozieDBCLI.java
+++ tools/src/test/java/org/apache/oozie/tools/TestOozieDBCLI.java
@@ -22,12 +22,22 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintStream;
+import java.io.Writer;
+import java.net.URI;
 import java.security.Permission;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.Path;
+import org.apache.oozie.service.HadoopAccessorService;
+import org.apache.oozie.service.ServiceException;
+import org.apache.oozie.service.Services;
+import org.apache.oozie.service.WorkflowAppService;
 import org.apache.oozie.test.XTestCase;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -35,6 +45,10 @@ import org.junit.BeforeClass;
 public class TestOozieDBCLI extends XTestCase {
     private SecurityManager SECURITY_MANAGER;
     private static String url = "jdbc:derby:target/test-data/oozietests/org.apache.oozie.tools.TestOozieDBCLI/data.db;create=true";
+    private String outPath = "outFolder";
+    private Services services = null;
+    private Path dstPath = null;
+    private  FileSystem fs ;
 
     @BeforeClass
     protected void setUp() throws Exception {
@@ -57,7 +71,6 @@ public class TestOozieDBCLI extends XTestCase {
 
     }
 
-
     private void execSQL(String sql) throws Exception {
         Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
         Connection conn = DriverManager.getConnection(url, "sa", "");
@@ -69,12 +82,148 @@ public class TestOozieDBCLI extends XTestCase {
         conn.close();
     }
 
-  
-    public void test1() throws Exception {
-        
-        File createSql=new File(getTestCaseConfDir() + File.separator + "out.sql");
-        String[] args = { "create", "-sqlfile",createSql.getAbsolutePath(), "-run" };
+    public void testOozieDBCLI() throws Exception {
         new LauncherSecurityManager();
+
+        File createSql = new File(getTestCaseConfDir() + File.separator + "out.sql");
+        String[] argsC = { "create", "-sqlfile", createSql.getAbsolutePath(), "-run" };
+        execOozieDBCLICommands(argsC);
+
+        assertTrue(createSql.exists());
+
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        PrintStream oldOut = System.out;
+        System.setOut(new PrintStream(data));
+
+        String[] argsv = { "version" };
+        execOozieDBCLICommands(argsv);
+
+        assertTrue(data.toString().contains("db.version: 1"));
+        System.setOut(oldOut);
+
+        File upgrage = new File(getTestCaseConfDir() + File.separator + "update.sql");
+        execSQL("DROP table OOZIE_SYS");
+        String[] argsu = { "upgrade", "-sqlfile", upgrage.getAbsolutePath(), "-run" };
+        execOozieDBCLICommands(argsu);
+
+        assertTrue(upgrage.exists());
+        File postupgrade = new File(getTestCaseConfDir() + File.separator + "postUpdate.sql");
+        String[] argspu = { "postupgrade", "-sqlfile", postupgrade.getAbsolutePath(), "-run" };
+        execOozieDBCLICommands(argspu);
+        assertTrue(postupgrade.exists());
+
+    }
+
+    public void testOozieSharelibCLI() throws Exception {
+        new LauncherSecurityManager();
+
+        String[] argsh = { "help" };
+        execOozieSharelibCLICommands(argsh);
+
+        File libDirectory = new File(getTestCaseConfDir() + File.separator + "lib");
+
+        if (!libDirectory.exists()) {
+            libDirectory.mkdirs();
+        }
+        else {
+            FileUtil.fullyDelete(libDirectory);
+            libDirectory.mkdirs();
+        }
+
+        writeFile(libDirectory, "file1", "test File");
+        writeFile(libDirectory, "file2", "test File2");
+
+        String[] argsc = { "create", "-fs", outPath, "-locallib", libDirectory.getParentFile().getAbsolutePath() };
+        execOozieSharelibCLICommands(argsc);
+
+        FileSystem fs = getTargetFileSysyem();
+
+        assertEquals(9, fs.getFileStatus(new Path(getDistPath(), "file1")).getLen());
+        assertEquals(10, fs.getFileStatus(new Path(getDistPath(), "file2")).getLen());
+
+    }
+
+    public void testOozieSharelibCLItarGz() throws Exception {
+        new LauncherSecurityManager();
+
+        String oozieHome = System.getProperty(OozieSharelibCLI.OOZIE_HOME);
+        File libDirectory = new File(oozieHome);
+
+        if (!libDirectory.exists()) {
+            libDirectory.mkdirs();
+        }
+
+        FileSystem fs = getTargetFileSysyem();
+        fs.delete(getDistPath(), true);
+
+        File source = new File("src/test/resources");
+        FileUtils.copyDirectory(source, libDirectory);
+
+        String[] argsc = { "create", "-fs", outPath };
+        execOozieSharelibCLICommands(argsc);
+
+        assertEquals(9, fs.getFileStatus(new Path(getDistPath(), "file1")).getLen());
+        assertEquals(10, fs.getFileStatus(new Path(getDistPath(), "file2")).getLen());
+
+    }
+
+    private FileSystem getTargetFileSysyem() throws Exception {
+if (fs==null){
+        HadoopAccessorService has = getServices().get(HadoopAccessorService.class);
+        URI uri = new Path(outPath).toUri();
+        Configuration fsConf = has.createJobConf(uri.getAuthority());
+         fs = has.createFileSystem(System.getProperty("user.name"), uri, fsConf);
+}
+        return fs;
+
+    }
+
+    private Services getServices() throws ServiceException {
+        if (services == null) {
+            services = new Services();
+            services.getConf().set(Services.CONF_SERVICE_CLASSES,
+                    "org.apache.oozie.service.LiteWorkflowAppService, org.apache.oozie.service.HadoopAccessorService");
+            services.init();
+        }
+        return services;
+    }
+
+    private Path getDistPath() throws Exception {
+        if (dstPath == null) {
+            WorkflowAppService lwas = getServices().get(WorkflowAppService.class);
+            dstPath = lwas.getSystemLibPath();
+        }
+        return dstPath;
+    }
+
+    private void writeFile(File folder, String filename, String content) throws Exception {
+        File file = new File(folder.getAbsolutePath() + File.separator + filename);
+        Writer writer = new FileWriter(file);
+        writer.write(content);
+        writer.flush();
+        writer.close();
+
+    }
+
+    private void execOozieSharelibCLICommands(String[] args) throws Exception {
+        try {
+            OozieSharelibCLI.main(args);
+        }
+        catch (SecurityException ex) {
+            if (LauncherSecurityManager.getExitInvoked()) {
+                System.out.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
+                System.err.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
+                if (LauncherSecurityManager.getExitCode() != 0) {
+                    fail();
+                }
+            }
+            else {
+                throw ex;
+            }
+        }
+    }
+
+    private void execOozieDBCLICommands(String[] args) {
         try {
             OozieDBCLI.main(args);
 
@@ -91,75 +240,7 @@ public class TestOozieDBCLI extends XTestCase {
                 throw ex;
             }
         }
-        assertTrue(createSql.exists());
-
-        ByteArrayOutputStream data = new ByteArrayOutputStream();
-        PrintStream oldOut = System.out;
-        System.setOut(new PrintStream(data));
-        try {
-            String[] argsv = { "version" };
-            OozieDBCLI.main(argsv);
-
-        }
-        catch (SecurityException ex) {
-            if (LauncherSecurityManager.getExitInvoked()) {
-                System.out.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
-                System.err.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
-                if (LauncherSecurityManager.getExitCode() != 0) {
-                    fail();
-                }
-            }
-            else {
-                throw ex;
-            }
-        }
-        assertTrue(data.toString().contains("db.version: 1"));
-        System.setOut(oldOut);
-
-        File upgrage = new File(getTestCaseConfDir() + File.separator + "update.sql");
-     
-        execSQL("DROP table OOZIE_SYS");
-
-        try {
-            String[] argsv = { "upgrade", "-sqlfile", upgrage.getAbsolutePath(), "-run" };
-            OozieDBCLI.main(argsv);
-
-        }
-        catch (SecurityException ex) {
-            if (LauncherSecurityManager.getExitInvoked()) {
-                System.out.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
-                System.err.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
-                if (LauncherSecurityManager.getExitCode() != 0) {
-                    fail();
-                }
-            }
-            else {
-                throw ex;
-            }
-        }
-        assertTrue(upgrage.exists());
-        File postupgrade=new File(getTestCaseConfDir() + File.separator + "postUpdate.sql");
-        try {
-            String[] argsv = { "postupgrade", "-sqlfile", postupgrade.getAbsolutePath(), "-run" };
-            OozieDBCLI.main(argsv);
-
-        }
-        catch (SecurityException ex) {
-            if (LauncherSecurityManager.getExitInvoked()) {
-                System.out.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
-                System.err.println("Intercepting System.exit(" + LauncherSecurityManager.getExitCode() + ")");
-                if (LauncherSecurityManager.getExitCode() != 0) {
-                    fail();
-                }
-            }
-            else {
-                throw ex;
-            }
-        }
-        assertTrue(postupgrade.exists());
-
     }
-
 }
 
 class LauncherSecurityManager extends SecurityManager {
