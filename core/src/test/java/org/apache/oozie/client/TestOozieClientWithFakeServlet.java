9890a976010082032ef4c5d2b330943a87603898
aleksgor <agorshkov@griddynamics.com>
2013-05-14 16:19:00 +0400

    format fix

--- core/src/test/java/org/apache/oozie/client/TestOozieClientWithFakeServlet.java
+++ core/src/test/java/org/apache/oozie/client/TestOozieClientWithFakeServlet.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.oozie.client;
 
 import java.io.ByteArrayInputStream;
@@ -21,71 +39,97 @@ import org.json.simple.JSONValue;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+
+
+/**
+ * Test some client functions with fake http connect 
+ *
+ */
 public class TestOozieClientWithFakeServlet {
 
     private int answer = 0;
-    private boolean check=true;
+    private boolean check = true;
 
+    /**
+     * Test method getJMSTopicName
+     */
     @Test
     public void testGetJMSTopicName() throws Exception {
-        answer=0;
-        check=true;
+        answer = 0;
+        check = true;
         FakeOozieClient client = new FakeOozieClient("http://url");
         String answer = client.getJMSTopicName("jobId");
         assertEquals("topicName", answer);
 
     }
+
+    /**
+     * Test method getJMSConnectionInfo
+     */
     @Test
     public void testGetJMSConnectionInfo() throws Exception {
-        answer=1;
-        check=true;
+        answer = 1;
+        check = true;
         FakeOozieClient client = new FakeOozieClient("http://url");
         JMSConnectionInfo answer = client.getJMSConnectionInfo();
         assertNotNull(answer);
 
     }
+
+    /**
+     * Test method getCoordActionInfo
+     */
     @Test
     public void testGetCoordActionInfo() throws Exception {
-        answer=1;
-        check=true;
+        answer = 1;
+        check = true;
         FakeOozieClient client = new FakeOozieClient("http://url");
         CoordinatorAction answer = client.getCoordActionInfo("actiomId");
         assertNotNull(answer);
-        
     }
+
+    /**
+     * Test method getBundleJobsInfo
+     */
     @Test
     public void testGetBundleJobsInfo() throws Exception {
-        answer=2;
-        check=true;
+        answer = 2;
+        check = true;
         FakeOozieClient client = new FakeOozieClient("http://url");
-        List<BundleJob>  answer = client.getBundleJobsInfo("", 0, 10);
+        List<BundleJob> answer = client.getBundleJobsInfo("", 0, 10);
         assertNotNull(answer);
-        assertEquals(1,answer.size());
-        
+        assertEquals(1, answer.size());
+
     }
-    
+
+    /**
+     * Test method getBulkInfo
+     */
     @Test
     public void testGetBulkInfo() throws Exception {
-        answer=3;
-        check=true;
+        answer = 3;
+        check = true;
         FakeOozieClient client = new FakeOozieClient("http://url");
-        List<BulkResponse>   answer = client.getBulkInfo("", 0, 10);
+        List<BulkResponse> answer = client.getBulkInfo("", 0, 10);
         assertNotNull(answer);
-        assertEquals(2,answer.size());
+        assertEquals(2, answer.size());
         assertEquals(Status.READY, answer.get(0).getAction().getStatus());
-        
+
     }
+
+    /**
+     * Test method FakeOozieClient
+     */
     @Test
     public void testBundleRerun() throws Exception {
-        answer=3;
-        check=true;
+        answer = 1;
+        check = true;
         FakeOozieClient client = new FakeOozieClient("http://url");
-       Void answer=  client.reRunBundle("jobId", "", "", true,true);
-       assertNull(answer);
+        Void answer = client.reRunBundle("jobId", "", "", true, true);
+        assertNull(answer);
 
     }
 
-    
     private class FakeOozieClient extends OozieClient {
 
         public FakeOozieClient(String oozieUrl) {
@@ -108,21 +152,21 @@ public class TestOozieClientWithFakeServlet {
                 array.put(2L);
                 String s = JSONValue.toJSONString(array);
                 result = new ByteArrayInputStream(s.getBytes());
-                check=false;
+                check = false;
                 return result;
             }
-            if(answer==0){
+            if (answer == 0) {
                 JSONObject json = new JSONObject();
                 json.put(JsonTags.JMS_TOPIC_NAME, "topicName");
                 result = new ByteArrayInputStream(json.toJSONString().getBytes());
 
             }
-            if(answer==1){
+            if (answer == 1) {
                 JSONObject json = new JSONObject();
                 result = new ByteArrayInputStream(json.toJSONString().getBytes());
 
             }
-            if(answer==2){
+            if (answer == 2) {
                 JSONObject json = new JSONObject();
                 List<WorkflowJobBean> jsonWorkflows = new ArrayList<WorkflowJobBean>();
                 jsonWorkflows.add(new WorkflowJobBean());
@@ -130,10 +174,10 @@ public class TestOozieClientWithFakeServlet {
                 result = new ByteArrayInputStream(json.toJSONString().getBytes());
 
             }
-            if(answer==3){
+            if (answer == 3) {
                 JSONObject json = new JSONObject();
-                List<BulkResponseImpl>  jsonWorkflows = new ArrayList<BulkResponseImpl>();
-                BulkResponseImpl bulk= new BulkResponseImpl();
+                List<BulkResponseImpl> jsonWorkflows = new ArrayList<BulkResponseImpl>();
+                BulkResponseImpl bulk = new BulkResponseImpl();
                 bulk.setBundle(new BundleJobBean());
                 bulk.setCoordinator(new CoordinatorJobBean());
                 CoordinatorActionBean action = new CoordinatorActionBean();
